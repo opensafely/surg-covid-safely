2dd40cd2b8de8ca6f24f2c89328eef8ff0558a69
Ciarán D. McInerney <44294573+ciaranmci@users.noreply.github.com>
2022-02-20 19:32:05 +0000

    Make data for Table 1

--- analysis/report.R
+++ analysis/report.R
@@ -47,7 +47,7 @@ myData <- myData %>%
             )
 myData <- myData %>%
             ## Indicator for 30-day post-operative mortality.
-           mutate(
+            mutate(
              postOp_mortality_30day = case_when(
               (.$date_death < .$date_surgery) ~ "Error: Surgery after death",
                 (.$date_death - .$date_surgery) <= 30 ~ "Dead within 30-day post-operation",
@@ -55,19 +55,39 @@ myData <- myData %>%
               is.na(.$date_death) ~ "No death recorded",
               is.na(.$date_surgery) ~ "No surgery recorded"
               )
-           ) %>%
+            ) %>%
             ## Month of surgery.
-           mutate(Month_surgery = lubridate::month(myData$date_surgery)) %>%
+            mutate(Month_surgery = lubridate::month(myData$date_surgery)) %>%
             ## Year of surgery.
-           mutate(Year_surgery = lubridate::year(myData$date_surgery)) %>%
+            mutate(Year_surgery = lubridate::year(myData$date_surgery)) %>%
             ## Indicator for pre-surgery COVID vaccination. 
-           mutate(
-            preSurgery_vaccinated = case_when(
-              (.$date_COVID_first_vaccination < .$date_surgery) ~ "PreSurgery vaccination",
-              (.$date_COVID_first_vaccination > .$date_surgery) ~ "PostSurgery vaccination",
-              is.na(.$date_COVID_first_vaccination) ~ "No vaccination recorded"
-             )
-           )
+            mutate(
+              preSurgery_vaccinated = case_when(
+                (.$date_COVID_first_vaccination < .$date_surgery) ~ "PreSurgery vaccination",
+                (.$date_COVID_first_vaccination > .$date_surgery) ~ "PostSurgery vaccination",
+                is.na(.$date_COVID_first_vaccination) ~ "No vaccination recorded"
+               )
+            ) %>%
+            ## No record of indication of pre-operative SARS-CoV-2 infection.
+            mutate(
+              preOperative_infection_status = case_when(
+                (.$date_surgery - .$date_latest_test_preOp_SARS_CoV_2_outcome_positive) <0 ~
+                    "Error: Test result after surgery. Check study_definition.",
+                !is.na(.$date_latest_test_preOp_SARS_CoV_2_outcome_positive) & 
+                  abs(.$date_surgery - .$date_latest_test_preOp_SARS_CoV_2_outcome_positive) < 14 ~ 
+                    "0-2 weeks record of pre-operative SARS-CoV-2 infection",
+                !is.na(.$date_latest_test_preOp_SARS_CoV_2_outcome_positive) & 
+                  dplyr::between(abs(.$date_surgery - .$date_latest_test_preOp_SARS_CoV_2_outcome_positive), 15, 28) ~ 
+                    "3-4 weeks record of pre-operative SARS-CoV-2 infection",
+                !is.na(.$date_latest_test_preOp_SARS_CoV_2_outcome_positive) & 
+                  dplyr::between(abs(.$date_surgery - .$date_latest_test_preOp_SARS_CoV_2_outcome_positive), 29, 42) ~ 
+                    "5-6 weeks record of pre-operative SARS-CoV-2 infection",
+                !is.na(.$date_latest_test_preOp_SARS_CoV_2_outcome_positive) & 
+                  abs(.$date_surgery - .$date_latest_test_preOp_SARS_CoV_2_outcome_positive) >= 49 ~ 
+                    ">=7 weeks record of pre-operative SARS-CoV-2 infection",
+                TRUE ~ "No record of pre-operative SARS-CoV-2 infection"
+              )
+            )
 
 
 # Collect plot data.
@@ -85,3 +105,66 @@ ggsave(
   filename="plot_postOp_mortality_30day.png",
   path=here::here("output"),
 )
+
+
+# Make Table 1.
+table1_ageGroup <- 
+    myData %>% group_by(surgery_pre_or_post_COVID_UK, age_group_surgery) %>%
+      summarise(n_all = sum(ifelse(preOperative_infection_status!=
+                                     "Error: Test result after surgery. Check study_definition.",1,0)),
+                n_infection_none = sum(ifelse(preOperative_infection_status==
+                                              "No record of pre-operative SARS-CoV-2 infection",1,0)),
+                n_infection_0to2wk = sum(ifelse(preOperative_infection_status==
+                                                "0-2 weeks record of pre-operative SARS-CoV-2 infection",1,0)),
+                n_infection_3to4wk = sum(ifelse(preOperative_infection_status==
+                                                "3-4 weeks record of pre-operative SARS-CoV-2 infection",1,0)),
+                n_infection_5to6wk = sum(ifelse(preOperative_infection_status==
+                                                "5-6 weeks record of pre-operative SARS-CoV-2 infection",1,0)),
+                n_infection_7wk = sum(ifelse(preOperative_infection_status==
+                                             ">=7 weeks record of pre-operative SARS-CoV-2 infection",1,0)),
+                prop_infection_none = n_infection_none/ n_all,
+                prop_infection_0to2wk = n_infection_0to2wk/ n_all,
+                prop_infection_3to4wk = n_infection_3to4wk/ n_all,
+                prop_infection_5to6wk = n_infection_5to6wk/ n_all,
+                prop_infection_7wk = n_infection_7wk/ n_all,
+                )
+table1_Sex <- 
+  myData %>% group_by(surgery_pre_or_post_COVID_UK,Sex) %>%
+  summarise(n_all = sum(ifelse(preOperative_infection_status!=
+                                 "Error: Test result after surgery. Check study_definition.",1,0)),
+            n_infection_none = sum(ifelse(preOperative_infection_status==
+                                            "No record of pre-operative SARS-CoV-2 infection",1,0)),
+            n_infection_0to2wk = sum(ifelse(preOperative_infection_status==
+                                              "0-2 weeks record of pre-operative SARS-CoV-2 infection",1,0)),
+            n_infection_3to4wk = sum(ifelse(preOperative_infection_status==
+                                              "3-4 weeks record of pre-operative SARS-CoV-2 infection",1,0)),
+            n_infection_5to6wk = sum(ifelse(preOperative_infection_status==
+                                              "5-6 weeks record of pre-operative SARS-CoV-2 infection",1,0)),
+            n_infection_7wk = sum(ifelse(preOperative_infection_status==
+                                           ">=7 weeks record of pre-operative SARS-CoV-2 infection",1,0)),
+            prop_infection_none = n_infection_none/ n_all,
+            prop_infection_0to2wk = n_infection_0to2wk/ n_all,
+            prop_infection_3to4wk = n_infection_3to4wk/ n_all,
+            prop_infection_5to6wk = n_infection_5to6wk/ n_all,
+            prop_infection_7wk = n_infection_7wk/ n_all,
+  )
+table1_postOp_mortality_30day <- 
+  myData %>% group_by(surgery_pre_or_post_COVID_UK, postOp_mortality_30day) %>%
+  summarise(n_all = sum(ifelse(preOperative_infection_status!=
+                                 "Error: Test result after surgery. Check study_definition.",1,0)),
+            n_infection_none = sum(ifelse(preOperative_infection_status==
+                                            "No record of pre-operative SARS-CoV-2 infection",1,0)),
+            n_infection_0to2wk = sum(ifelse(preOperative_infection_status==
+                                              "0-2 weeks record of pre-operative SARS-CoV-2 infection",1,0)),
+            n_infection_3to4wk = sum(ifelse(preOperative_infection_status==
+                                              "3-4 weeks record of pre-operative SARS-CoV-2 infection",1,0)),
+            n_infection_5to6wk = sum(ifelse(preOperative_infection_status==
+                                              "5-6 weeks record of pre-operative SARS-CoV-2 infection",1,0)),
+            n_infection_7wk = sum(ifelse(preOperative_infection_status==
+                                           ">=7 weeks record of pre-operative SARS-CoV-2 infection",1,0)),
+            prop_infection_none = n_infection_none/ n_all,
+            prop_infection_0to2wk = n_infection_0to2wk/ n_all,
+            prop_infection_3to4wk = n_infection_3to4wk/ n_all,
+            prop_infection_5to6wk = n_infection_5to6wk/ n_all,
+            prop_infection_7wk = n_infection_7wk/ n_all,
+  )
