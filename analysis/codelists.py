dbd9ae82652af9bbedf8c7116e38c2253b18ab38
Ciarán D. McInerney <44294573+ciaranmci@users.noreply.github.com>
2022-05-12 17:15:49 +0100

    Begin next round of covariates for stratification

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -193,7 +193,7 @@ codelist_cancer_ctv3_combine = combine_codelists(
 #    system="snomed",
 #    column="code",
 #)
-### Post-operative **<indicator of post-operative cardcerebrovascular iac #complications>.
+### Post-operative **<indicator of post-operative cerebrovascular  #complications>.
 #codelist_** = codelist_from_csv(
 #    "codelists/codelist-*.csv",
 #    system="snomed",
@@ -204,98 +204,36 @@ codelist_cancer_ctv3_combine = combine_codelists(
 #    codelist_*,
 #    codelist_**,
 #)
-#
-#
-## #####################
-## Covariates
-## #####################
-## ## Outcomes used in the original COVIDSurg study.
+
+
+################
+## Covariates ##
+################
 ## Revised Cardiac Risk Index
-# Defined by the sum of 1/0 indications of:
-# - Current surgeryis intraperitoneal or intrathoracic;
-# - History of ischemic heart disease;
-# - History of congestive heart failure;
-# - History of cerebrovascular disease;
-# - History of diabetes mellitus treated with insulin;
-# - History of chronic kidney disease (creatinine > 176 μmol.l-1).
-#
-## Intraperioneal surgery.
-#codelist_intraperioneal_surgery = codelist_from_csv(
-#    "codelists/codelist-intraperioneal-surgery.csv",
-#    system="snomed",
-#    column="code",
-#)
-## Intrathoracic surgery.
-#codelist_intrathoracic_surgery = codelist_from_csv(
-#    "codelists/codelist-intrathoracic-surgery.csv",
-#    system="snomed",
-#    column="code",
-#)
-## Intraperioneal or intrathoracic surgery.
-#codelist_intraperioneal_or_intrathoracic_surgery = codelist_combine(
-#    codelist_intraperioneal_surgery,
-#    codelist_intrathoracic_surgery,
-#)
-## Ischedmic heart disease.
-#codelist_ischemic_heart_disease = codelist_from_csv(
-#    "codelists/codelist-ischemic-heart-disease.csv",
-#    system="snomed",
-#    column="code",
-#)
-## Congestive heart failure.
-#codelist_congestive_heart_failure = codelist_from_csv(
-#    "codelists/codelist-congestive-heart-failure.csv",
-#    system="snomed",
-#    column="code",
-#)
-## Cerebrovascular disease.
-#codelist_cerebrovascular_disease = codelist_from_csv(
-#    "codelists/codelist-cerebrovascular-disease.csv",
-#    system="snomed",
-#    column="code",
-#)
-## Diabetes mellitus treated with insulin.
-#codelist_diabetes_mellitus_treated_with_insulin = codelist_from_csv(
-#    "codelists/codelist-diabetes-mellitus-treated-with-insulin.csv",
-#    system="snomed",
-#    column="code",
-#)
-## Chronic kidney disease, CKD.
-#codelist_chronic_kidney_disease = codelist_from_csv(
-#    "codelists/codelist-chronic-kidney-disease.csv",
-#    system="snomed",
-#    column="code",
-#)
-#
-### Presence of respiratory comorbidities.
-## Asthma.
-#codelist_asthma = codelist_from_csv(
-#  "codelists/opensafely-asthma-diagnosis-snomed.csv"
-#  system="snomed",
-#  colum="code",
-#)
-## COPD.
-#codelist_COPD = codelist_from_csv(
-#  "codelists/opensafely-chronic-obstructive-pulmonary-disease-copd-review-#qof.csv",
-#  system="snomed",
-#  colum="code",
-#)
+# Where COVIDSurg used the Revised Cardiac Risk Index, we 
+# stratify based on the component conditions.
+
+# Chronic cardiac disease.
+codelist_chronic_cardiac_disease = codelist_from_csv(
+   "codelists/opensafely-chronic-cardiac-disease-snomed.csv",
+   system="snomed",
+   column="id",
+)
+# Diabetes.
+codelist_diabetes = codelist_from_csv(
+   "codelists/opensafely-diabetes-snomed.csv",
+   system="snomed",
+   column="id",
+)
+
 ## Presence of respiratory comorbidities.
-#codelist_respiratory_comorbidities = combine_codelists(
-#    codelist_asthma,
-#    codelist_COPD,
-#)
-#
-### BUPA grade of surgery.
-## Minor codes.
-#codelist_BUPA_grade_of_surgery_minor = codelist_from_csv(
-#   "codelists/codelist-BUPA-grade-of-surgery-minor.csv",
-#    system = "snomed",
-#    column = "code",
-#)
-## Major codes.
-#codelist_BUPA_grade_of_surgery_major = codelist_from_csv(
-#   "codelists/codelist-BUPA-grade-of-surgery-major.csv",
-#    system = "snomed",
-#    column = "code",
-#)
+# Where COVIDSurg defined the presence of respiratory comorbidities
+# as the presence of either asthma or COPD, we use use an OpenSAFELY
+# codelist for chronic respiratory disease.
+
+# Chronic respiratory disease.
+codelist_chronic_respiratory_disease = codelist_from_csv(
+ "codelists/opensafely-chronic-respiratory-disease.csv",
+ system="ctv3",
+ column="CTV3ID",
+)
