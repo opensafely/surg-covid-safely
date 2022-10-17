47628f4387791d3f2b53d048f2aededf718306d1
Ciarán D. McInerney <44294573+ciaranmci@users.noreply.github.com>
2022-10-17 11:30:17 +0100

    Update fnc_countsAndPercentages.R
    Update intended to fix Inf issues that is only happening on the main server (i.e. "non-numeric argument to binary operator")

--- analysis/fnc_countsAndPercentages.R
+++ analysis/fnc_countsAndPercentages.R
@@ -45,14 +45,15 @@ fnc_countsAndPercentages <-
       n <- i_table_to_use %>% dplyr::select(-c("era", strata))
       # Get percentages per intervals and overall.
       pct <- 
-        n %>% mapply('/', ., i_counts_to_use) %>% '*'(100) %>%
+        n %>% mapply('/', ., i_counts_to_use) %>% tibble::as_tibble() %>%
+        replace(., . == Inf, 0) %>%
+        '*'(100) %>%
         tibble::as_tibble() %>%
         dplyr::mutate(dplyr::across(.cols = everything(), .fns = ~ ifelse(is.nan(.), NA,.))) %>%
         tidyr::replace_na(list("n_all_intervals" = 0, "n_infection_none" = 0,
                                "n_infection_0to2wk" = 0, "n_infection_3to4wk" = 0,
                                "n_infection_5to6wk" = 0, "n_infection_7wk" = 0
         )) %>%
-        replace(., . == Inf, 0) %>%
         `colnames<-`(c("pct_all_intervals", "pct_infection_none", "pct_infection_0to2wk",
                        "pct_infection_3to4wk", "pct_infection_5to6wk", "pct_infection_7wk"))
       # Interlace counts and percentages.
