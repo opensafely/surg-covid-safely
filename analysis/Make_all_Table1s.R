e477f493a0e7717e31c7065187b339ed9cbf0fba
Ciarán D. McInerney <44294573+ciaranmci@users.noreply.github.com>
2022-10-17 14:46:57 +0100

    Update Make_all_Table1s.R

--- analysis/Make_all_Table1s.R
+++ analysis/Make_all_Table1s.R
@@ -25,36 +25,50 @@ data_to_use_C_outwith3m <- myDataSelect %>% dplyr::filter(has_surgery == TRUE) %
 data_to_use_C_within6m <- myDataSelect %>% dplyr::filter(has_surgery == TRUE) %>% 
   dplyr::filter(has_cancer == TRUE) %>%
   dplyr::filter(category_cancer_within_6mths_surgery == 
-                  "Cancer diagnosis within 3mths before surgery" |
+                  "Cancer diagnosis within 6mths before surgery" |
                   category_cancer_within_6mths_surgery == 
-                  "Cancer diagnosis within 3mths after surgery")
+                  "Cancer diagnosis within 6mths after surgery")
 source(here::here("analysis","Make_table_COVIDSurg_compare.R"))
 # Make Table1 for all surgery patients.
+message("\nMaking Table1 for all surgery patients...")
 data_to_use <- data_to_use_all
 sensitivity_cohort <- "all"
 source(here::here("analysis","Make_Table1.R"))
 rm(data_to_use, sensitivity_cohort)
+message("\n\t\t...table complete.\n\n")
 # Make Table1 for patients who do not have a cancer diagnosis
 # since the start of our data collection period (17th March 2018).
+message(paste0("\nMaking Table1 for patients who do not have a cancer diagnosis ",
+               "since the start of our data collection period (17th March 2018)..."))
 data_to_use <- data_to_use_NC
 sensitivity_cohort <- "NC"
 source(here::here("analysis","Make_Table1.R"))
 rm(data_to_use, sensitivity_cohort)
+message("\n\t\t...table complete.\n\n")
 # Make Table1 for patients whose cancer diagnosis was within
 # 3 months of their surgery.
+message(paste0("\nMaking Table1 for patients whose cancer diagnosis was within ",
+               "3 months of their surgery...."))
 data_to_use <- data_to_use_C_within3m
 sensitivity_cohort <- "C_within3m"
 source(here::here("analysis","Make_Table1.R"))
 rm(data_to_use, sensitivity_cohort)
+message("\n\t\t...table complete.\n\n")
 # Make Table1 for patients whose cancer diagnosis was beyond
 # 3 months of their surgery.
+message(paste0("\nMaking Table1 for patients whose cancer diagnosis was beyond ",
+               "3 months of their surgery...."))
 data_to_use <- data_to_use_C_outwith3m
 sensitivity_cohort <- "C_outwith3m"
 source(here::here("analysis","Make_Table1.R"))
 rm(data_to_use, sensitivity_cohort)
+message("\n\t\t...table complete.\n\n")
 # Make Table1 for patients whose cancer diagnosis was within
 # 6 months of their surgery.
+message(paste0("\nMaking Table1 for patients whose cancer diagnosis was within ",
+               "6 months of their surgery...."))
 data_to_use <- data_to_use_C_within6m
 sensitivity_cohort <- "C_within6m"
 source(here::here("analysis","Make_Table1.R"))
 rm(data_to_use, sensitivity_cohort)
+message("\n\t\t...table complete.\n\n")
