6f8c2977f6e78e7b2298a412e71d2f91c0b0d408
Ciarán D. McInerney <44294573+ciaranmci@users.noreply.github.com>
2022-10-21 11:44:13 +0100

    Remove redefinition of data subsets
    I was redefining the data_to_use_* data subsets, multiple times. Now, they are all pull from the first definition make in Make_table_COVIDSurg_compare.R

--- analysis/Make_all_Table1s.R
+++ analysis/Make_all_Table1s.R
@@ -1,33 +1,5 @@
 
 source(here::here("analysis","dataset_preparation.R"))
-
-myDataSelect <- myData %>%
-  dplyr::filter(postOp_mortality_30day %in% c("Dead within 30 days post-operation",
-                                              "Alive within 30 days post-operation",
-                                              "No death recorded"),
-                preOperative_infection_status!=
-                  "Error: Test result after surgery. Check study_definition.",
-                era != "No surgery date recorded",
-                COVIDSurg_data_collection_period != "No surgery date recorded")
-data_to_use_all <- myDataSelect %>% dplyr::filter(has_surgery == TRUE)
-data_to_use_NC <- myDataSelect %>% dplyr::filter(has_surgery == TRUE) %>%
-  dplyr::filter(has_cancer == FALSE)
-data_to_use_C_within3m <- myDataSelect %>% dplyr::filter(has_surgery == TRUE) %>% 
-  dplyr::filter(has_cancer == TRUE) %>%
-  dplyr::filter(category_cancer_within_3mths_surgery == 
-                  "Cancer diagnosis within 3mths before surgery" |
-                  category_cancer_within_3mths_surgery == 
-                  "Cancer diagnosis within 3mths after surgery")
-data_to_use_C_outwith3m <- myDataSelect %>% dplyr::filter(has_surgery == TRUE) %>% 
-  dplyr::filter(has_cancer == TRUE) %>%
-  dplyr::filter(category_cancer_within_3mths_surgery == 
-                  "No cancer diagnosis within 3mths before or after surgery")
-data_to_use_C_within6m <- myDataSelect %>% dplyr::filter(has_surgery == TRUE) %>% 
-  dplyr::filter(has_cancer == TRUE) %>%
-  dplyr::filter(category_cancer_within_6mths_surgery == 
-                  "Cancer diagnosis within 6mths before surgery" |
-                  category_cancer_within_6mths_surgery == 
-                  "Cancer diagnosis within 6mths after surgery")
 source(here::here("analysis","Make_table_COVIDSurg_compare.R"))
 # Make Table1 for all surgery patients.
 message("\nMaking Table1 for all surgery patients...")
